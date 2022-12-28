855666db4d345c84fee9dc92082cccddc4fec7bb
liqun.tang <liqun.tang@rakuten.com>
2022-12-28 16:24:28 +0900

    callback style method not needed, it is inferred

--- multiplier/handler.js
+++ multiplier/handler.js
@@ -7,11 +7,12 @@ class MultiplyHandler {
     console.log('multiplying', n1, n2);
     return n1 * n2;
   }
-  multiply(n1, n2, cb) {
-    console.log('multiplying with callback', n1, n2);
+  // callback style not actually needed, thrift just handles it automatically for both promise and callback style methods
+  //   multiply(n1, n2, cb) {
+  //     console.log('multiplying with callback', n1, n2);
 
-    return cb(null, n1 * n2);
-  }
+  //     return cb(null, n1 * n2);
+  //   }
 }
 
 module.exports = {
